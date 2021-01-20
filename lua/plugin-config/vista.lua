acc3a54e7467e1c2c3c1b44e2f6ad65ae23f8ee2
yongqi <77aimiao@gmail.com>
2021-01-20 11:41:29 +0800

    feat: remoe gtag

--- lua/plugin-config/vista.lua
+++ lua/plugin-config/vista.lua
@@ -1,15 +1,10 @@
 return function()
   vim.g.vista_disable_statusline = 1
-  vim.g.vista_default_executive = "ctags"
+  vim.g.vista_default_executive = "nvim_lsp"
   vim.g.vista_echo_cursor_strategy = "scroll"
   vim.g.vista_vimwiki_executive = "markdown"
   vim.g.vista_executive_for = {
     vimwiki = "markdown",
     pandoc = "markdown",
-    markdown = "vim_lsp",
-    typescript = "nvim_lsp",
-    typescriptreact = "nvim_lsp",
-    vue = "nvim_lsp",
-    lua = "nvim_lsp"
   }
 end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -125,10 +125,6 @@ return packer.startup(
     }
 
     use {
-      "ludovicchabant/vim-gutentags",
-      config = require("plugin-config.vim-gutentags")
-    }
-    use {
       "liuchengxu/vista.vim",
       config = require("plugin-config.vista")
     }
