dac2c3b390f775b25d22df28e463129951a228ab
yongqi <77aimiao@gmail.com>
2021-01-29 11:54:25 +0800

    update plugin config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -147,6 +147,7 @@ return packer.startup(
           config = function()
             vim.g.vsnip_filetypes = {}
             vim.g.vsnip_filetypes.javascriptreact = {"javascript"}
+            vim.g.vsnip_filetypes.vue = {"vue", "javascript", "typescript"}
             vim.g.vsnip_filetypes.typescriptreact = {"typescript"}
           end
         },
