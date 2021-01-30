8a12f049202ca92f6f5975412a34a1a7ae168b0d
yongqi <77aimiao@gmail.com>
2021-01-30 17:43:39 +0800

    update config

--- lua/plugin-config/vista.lua
+++ lua/plugin-config/vista.lua
@@ -1,7 +1,7 @@
 return function()
   vim.g.vista_disable_statusline = 1
   vim.g.vista_default_executive = "nvim_lsp"
-  vim.g.vista_echo_cursor_strategy = "both"
+  vim.g.vista_echo_cursor_strategy = "scroll"
   vim.g.vista_vimwiki_executive = "markdown"
   vim.g.vista_highlight_whole_line = 1
   vim.g.vista_executive_for = {
