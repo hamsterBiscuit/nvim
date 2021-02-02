768aa88dd5361c7e9c61b370f38122aec95db5cf
yongqi <77aimiao@gmail.com>
2021-02-02 11:06:19 +0800

    remove plugin config

--- lua/plugin-config/zephyr.lua
+++
@@ -1,5 +0,0 @@
-  require("zephyr")
-  local syntax = require "zephyr".get_zephyr_color()
-  vim.api.nvim_command("highlight htmlTagName gui=bold guifg=" .. syntax.orange .. " guibg=NONE cterm=bold")
-  vim.api.nvim_command("highlight htmlArg gui=NONE guifg=" .. syntax.blue .. " guibg=NONE cterm=bold")
-  vim.api.nvim_command("highlight htmlTag gui=NONE guifg=" .. syntax.blue .. " guibg=NONE cterm=bold")
