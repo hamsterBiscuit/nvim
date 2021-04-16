872dec3249967b9b7807493dd907691905478318
yongqi <77aimiao@gmail.com>
2021-04-16 16:17:50 +0800

    fix: tree

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -1,20 +1,22 @@
-  vim.g.nvim_tree_follow = 1
-  vim.g.nvim_tree_indent_markers = 1
-  vim.g.nvim_tree_auto_close = 1
-  vim.g.nvim_tree_allow_resize = 1
-  vim.g.nvim_tree_bindings = {
-    ["l"] = ":lua require'nvim-tree'.on_keypress('edit')<CR>",
-    ["s"] = ":lua require'nvim-tree'.on_keypress('vsplit')<CR>",
-    ["i"] = ":lua require'nvim-tree'.on_keypress('split')<CR>",
+vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_allow_resize = 1
+vim.g.nvim_tree_auto_open = 1
+vim.g.nvim_tree_bindings = {
+  ["l"] = ":lua require'nvim-tree'.on_keypress('edit')<CR>",
+  ["s"] = ":lua require'nvim-tree'.on_keypress('vsplit')<CR>",
+  ["i"] = ":lua require'nvim-tree'.on_keypress('split')<CR>"
+}
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "✚",
+    staged = "✚",
+    unmerged = "≠",
+    renamed = "≫",
+    untracked = "★"
   }
-  vim.g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-      unstaged = "✚",
-      staged = "✚",
-      unmerged = "≠",
-      renamed = "≫",
-      untracked = "★"
-    }
-  }
+}
+require "nvim-tree".on_enter()
