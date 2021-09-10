7e38555b9dbc691c9888e4fee12de44b8c06d27e
yongqi <77aimiao@gmail.com>
2021-09-10 15:54:40 +0800

    feat: update some config

--- lua/plugin-config/delimitMate.lua
+++ lua/plugin-config/delimitMate.lua
@@ -2,5 +2,5 @@
   vim.g.delimitMate_expand_space = 1
   vim.g.delimitMate_smart_quotes = 1
   vim.g.delimitMate_expand_inside_quotes = 0
-  vim.api.nvim_command([[au FileType xml,html,phtml,php,xhtml,js let b:delimitMate_matchpairs = "(:),[:],{:}"]])
+  vim.api.nvim_command([[au FileType xml,html,phtml,php,xhtml,js,ts let b:delimitMate_matchpairs = "(:),[:],{:}"]])
 
