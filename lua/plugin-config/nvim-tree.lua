10dd26eebfa6e9ce171ad04de21637d3345b3f34
yongqi <77aimiao@gmail.com>
2022-04-02 09:58:43 +0800

    feat: update nvim tree auto close

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -10,10 +10,10 @@ vim.g.nvim_tree_show_icons = {
   files = 1
 }
 vim.g.nvim_tree_git_gl = 0
+vim.cmd[[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]]
 
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 require("nvim-tree").setup {
-  auto_close = true,
   update_focused_file = {
     -- update_cwd = true
   },
