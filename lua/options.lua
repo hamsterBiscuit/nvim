962707d332ce114e29405207aaaee278ed33c18f
yongqi <77aimiao@gmail.com>
2022-04-14 17:03:31 +0800

    feat: update config

--- lua/options.lua
+++ lua/options.lua
@@ -117,5 +117,13 @@ function options:load_options()
   end
   bind.bind_option(self.bw_local)
 end
+-- https://stackoverflow.com/questions/44480829/how-to-copy-to-clipboard-in-vim-of-bash-on-windows
+vim.cmd([[
+augroup fix_yank
+    autocmd!
+    autocmd TextYankPost * if v:event.operator ==# 'y' | call system('/mnt/c/Windows/System32/clip.exe', @0) | endif
+augroup END
+]])
+
 
 return options
