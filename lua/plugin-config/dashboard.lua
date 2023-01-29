e2611f16f6fecf9c16d172e07dee7ee87d85d965
yongqi <zhangyongqi@leying365.com>
2023-01-29 23:36:10 +0800

    feat: update

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -1,45 +1,32 @@
-local home = os.getenv("HOME")
-local z = require('zephyr')
 local db = require("dashboard")
-db.preview_command = "cat | lolcat -F 0.3"
-db.preview_file_path = home .. "/.config/nvim/static/neovim.cat"
-db.preview_file_height = 12
-db.preview_file_width = 80
-db.custom_center = {
+db.setup(
   {
-    icon = "  ",
-    desc = "Update Plugins                          ",
-    shortcut = "SPC p u",
-    action = "PackerUpdate"
-  },
-  {
-    icon = "  ",
-    desc = "Recently opened files                   ",
-    action = "Telescope oldfiles",
-    shortcut = "SPC f h"
-  },
-  {
-    icon = "  ",
-    desc = "Find  File                              ",
-    action = "Telescope find_files find_command=rg,--hidden,--files",
-    shortcut = "SPC f f"
-  },
-  {
-    icon = "  ",
-    desc = "File Browser                            ",
-    action = "Telescope file_browser",
-    shortcut = "SPC f b"
-  },
-  {
-    icon = "  ",
-    desc = "Find  word                              ",
-    action = "Telescope live_grep",
-    shortcut = "SPC f b"
-  },
-  {
-    icon = "  ",
-    desc = "Open Personal dotfiles                  ",
-    action = "Telescope dotfiles path=" .. home .. "/.dotfiles",
-    shortcut = "SPC f d"
+    theme = "hyper",
+    config = {
+      week_header = {
+        enable = true
+      },
+      shortcut = {
+        {desc = " Update", group = "@property", action = "Lazy update", key = "u"},
+        {
+          desc = " Files",
+          group = "Label",
+          action = "Telescope find_files",
+          key = "f"
+        },
+        {
+          desc = " Apps",
+          group = "DiagnosticHint",
+          action = "Telescope app",
+          key = "a"
+        },
+        {
+          desc = " dotfiles",
+          group = "Number",
+          action = "Telescope dotfiles",
+          key = "d"
+        }
+      }
+    }
   }
-}
+)
