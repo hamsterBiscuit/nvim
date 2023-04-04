0d74ec2f21ad10797528144d2010f09bf33a6a6f
yongqi <zhangyongqi@leying365.com>
2023-04-04 19:05:23 +0800

    feat: update

--- lua/plugin-config/bufferline.lua
+++ lua/plugin-config/bufferline.lua
@@ -1,7 +1,26 @@
-require "bufferline".setup {
-  options = {
-    -- modified_icon = "✥",
-    -- buffer_close_icon = "",
-    always_show_bufferline = true
+local status, bufferline = pcall(require, "bufferline")
+if not status then
+  vim.notify("not found bufferline")
+  return
+end
+bufferline.setup(
+  {
+    options = {
+      -- To close the Tab command, use moll/vim-bbye's :Bdelete command here
+      close_command = "Bdelete! %d",
+      right_mouse_command = "Bdelete! %d",
+      -- Using nvim's built-in LSP will be configured later in the course
+      diagnostics = "nvim_lsp",
+      -- Optional, show LSP error icon
+      ---@diagnostic disable-next-line: unused-local
+      diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        local s = " "
+        for e, n in pairs(diagnostics_dict) do
+          local sym = e == "error" and " " or (e == "warning" and " " or "")
+          s = s .. n .. sym
+        end
+        return s
+      end
+    }
   }
-}
+)
