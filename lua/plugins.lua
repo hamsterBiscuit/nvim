24e346d0ddb04ef150d935c4c95b1b6b37ddf8bc
yongqi <zhangyongqi@leying365.com>
2023-06-30 12:14:14 +0800

    feat: update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -155,43 +155,43 @@ require("lazy").setup(
     -- f t
     {
       "ggandor/lightspeed.nvim",
-      keys = {
-        "<Plug>Lightspeed_s",
-        "<Plug>Lightspeed_S",
-        "<Plug>Lightspeed_x",
-        "<Plug>Lightspeed_X",
-        "<Plug>Lightspeed_f",
-        "<Plug>Lightspeed_F",
-        "<Plug>Lightspeed_t",
-        "<Plug>Lightspeed_T"
-      },
-      config = function()
-        local default_keymaps = {
-          {"n", "s", "<Plug>Lightspeed_s"},
-          {"n", "S", "<Plug>Lightspeed_S"},
-          {"x", "s", "<Plug>Lightspeed_s"},
-          {"x", "S", "<Plug>Lightspeed_S"},
-          {"o", "z", "<Plug>Lightspeed_s"},
-          {"o", "Z", "<Plug>Lightspeed_S"},
-          {"o", "x", "<Plug>Lightspeed_x"},
-          {"o", "X", "<Plug>Lightspeed_X"},
-          {"n", "f", "<Plug>Lightspeed_f"},
-          {"n", "F", "<Plug>Lightspeed_F"},
-          {"x", "f", "<Plug>Lightspeed_f"},
-          {"x", "F", "<Plug>Lightspeed_F"},
-          {"o", "f", "<Plug>Lightspeed_f"},
-          {"o", "F", "<Plug>Lightspeed_F"},
-          {"n", "t", "<Plug>Lightspeed_t"},
-          {"n", "T", "<Plug>Lightspeed_T"},
-          {"x", "t", "<Plug>Lightspeed_t"},
-          {"x", "T", "<Plug>Lightspeed_T"},
-          {"o", "t", "<Plug>Lightspeed_t"},
-          {"o", "T", "<Plug>Lightspeed_T"}
-        }
-        for _, m in ipairs(default_keymaps) do
-          vim.api.nvim_set_keymap(m[1], m[2], m[3], {silent = true})
-        end
-      end
+      -- keys = {
+      --   "<Plug>Lightspeed_s",
+      --   "<Plug>Lightspeed_S",
+      --   "<Plug>Lightspeed_x",
+      --   "<Plug>Lightspeed_X",
+      --   "<Plug>Lightspeed_f",
+      --   "<Plug>Lightspeed_F",
+      --   "<Plug>Lightspeed_t",
+      --   "<Plug>Lightspeed_T"
+      -- },
+      -- config = function()
+      --   local default_keymaps = {
+      --     {"n", "s", "<Plug>Lightspeed_s"},
+      --     {"n", "S", "<Plug>Lightspeed_S"},
+      --     {"x", "s", "<Plug>Lightspeed_s"},
+      --     {"x", "S", "<Plug>Lightspeed_S"},
+      --     {"o", "z", "<Plug>Lightspeed_s"},
+      --     {"o", "Z", "<Plug>Lightspeed_S"},
+      --     {"o", "x", "<Plug>Lightspeed_x"},
+      --     {"o", "X", "<Plug>Lightspeed_X"},
+      --     {"n", "f", "<Plug>Lightspeed_f"},
+      --     {"n", "F", "<Plug>Lightspeed_F"},
+      --     {"x", "f", "<Plug>Lightspeed_f"},
+      --     {"x", "F", "<Plug>Lightspeed_F"},
+      --     {"o", "f", "<Plug>Lightspeed_f"},
+      --     {"o", "F", "<Plug>Lightspeed_F"},
+      --     {"n", "t", "<Plug>Lightspeed_t"},
+      --     {"n", "T", "<Plug>Lightspeed_T"},
+      --     {"x", "t", "<Plug>Lightspeed_t"},
+      --     {"x", "T", "<Plug>Lightspeed_T"},
+      --     {"o", "t", "<Plug>Lightspeed_t"},
+      --     {"o", "T", "<Plug>Lightspeed_T"}
+      --   }
+      --   for _, m in ipairs(default_keymaps) do
+      --     vim.api.nvim_set_keymap(m[1], m[2], m[3], {silent = true})
+      --   end
+      -- end
     },
     -- 平滑滚动插件 半屏或者整屏翻页变为滚动效果
     {
